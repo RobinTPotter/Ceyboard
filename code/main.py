f05670a5494df9c0dfe690c0363e2344138031cd
Robin Potter <Robin.Potter@experian.com>
2025-05-16 19:39:59 +0100

    upd

--- code/main.py
+++ code/main.py
@@ -1,65 +1,48 @@
 import time
+import utime
 from machine import Pin, PWM
 from random import random
+import math
 
-class PWMPLayer():
-    def __init__(self, pin_num=0, freq=440.0, freq_offset=0, freq_mult=1.0, transpose=0, freq_random_mult=0, duty_mult=1.0, duty_offset=0):
-        self.pwm = PWM(Pin(pin_num, Pin.OUT))
-        self.freq_offset = freq_offset
-        self.freq_mult = freq_mult
-        self.freq = freq
-        self.transpose = transpose
-        self.freq_random_mult = freq_random_mult
-        self.duty_mult = duty_mult
-        self.duty_offset = duty_offset
-    def duty_u16(self, duty):
-        self.duty = int(duty * self.duty_mult + self.duty_offset)
-    def on(self):
-        self.pwm.duty_u16(self.duty)
-    def off(self):
+WAVE_TABLE_SIZE = 100
+SAMPLE_RATE = 4000
+
+class PWMPlayer:
+    def __init__(self, pin):
+        self.pwm = Pin(pin, Pin.OUT)
+        self.pwm.freq(62500)
         self.pwm.duty_u16(0)
+        self.on = True
+        self.amp = 1.0
+        self.index = 0
+        self.step = 0
     def freq(self, freq):
-        self.freq = freq
-	self.set_freq()
-    def transpose(self, semitone):
-        self.transpose = self.transpose + semitone 
-	self.set_freq()
-    def set_freq(self)
-        fr = int(
-            (self.freq * 2**(1/12) * self.transpose ) * self.freq_mult
-            + self.freq_offset
-            + random()*self.freq_random_mult
-        )
-        self.pwm.freq(fr)
+        self.step = freq * WAVE_TABLE_SIZE / SAMPLE_RATE
 
+channels = [PWMPlayer(pin) for pin in [0,1,2,3,4,5]]  #6 channels
 
+ff = 440.0
+for i,ch in enumerate(channels):
+    ch.freq(440 * (5+i)/5)
 
-vol = 100.0
+# generate wave table
+wave = [
+    int( 65535 * (1 + math.sin(2 * math.pi * i/WAVE_TABLE_SIZE) ) / 2 ) for i in range(WAVE_TABLE_SIZE)
+]
 
+interval_ms = 1000000 / SAMPLE_RATE
 
-pwm = PWMPlayer(2)
-pwm.freq(440.0)
-pwm.duty_u16(int(32000.0 * vol / 100))
-pwm.on()
-time.sleep(0.5)
-pwm.off()
-time.sleep(0.5)
-
-left = [2,6,12]
-right = [16, 20, 28]
-
-left = [PWMPlayer(l) for l in left]
-right = [PWMPlayer(r) for r in right]
-all = left + right
-for a,t in zip(all,[0,4,7,11,14,21]):
-    a.transpose(t)
-    a.on()
-    time.sleep(0.2)
-
-time.sleep(0.5)
-
-for a in all: a.off()
-
-
+for _ in range(3 * SAMPLE_RATE):
+    for ch in channels:
+        if ch.on:
+            index = ch.index
+            value = int(ch.amp * wave[int(index) % WAVE_TABLE_SIZE])
+            ch.pwm.duty_u16(value)
+            ch.index = ( index + ch.step ) % WAVE_TABLE_SIZE
+        else:
+            ch.pwm.duty_u16(0)
+            
+    utime.sleep_us(interval_ms)
+        
 
 
